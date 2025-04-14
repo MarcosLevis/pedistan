8088722f0b54a0ee22246f2acb3d587505fc3843
MarcosLevis <marcoslevis.rck@gmail.com>
2025-04-14 17:10:50 -0300

    conexion a bd sequalize y controller

--- backend/src/routes/ligas.ts
+++ backend/src/routes/ligas.ts
@@ -1,9 +1,15 @@
 import { Request, Response, Router } from "express"
-import { LigaController } from "../controllers/liga.controller"
+import { getLigas,postLiga } from "../controllers/liga.controller"
+
 // import { getLigas,getLiga,postLiga,updateLiga,deleteLiga } from "../controllers/liga.controller"
 const router = Router()
 
 //le saco el /ligas por que ya el url va a tener el nombre del archivo entonces cuando haga un get me trae todas las ligas
-router.get('/', LigaController.getAll)
+
+
+
+router.get('/', getLigas)
+router.post('/', postLiga)
+
 
 export { router }
